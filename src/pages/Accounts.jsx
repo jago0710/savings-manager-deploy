5725d8339e64311d282a2aa91d404510337c5913
jago0710 <55157653l@iespoblenou.org>
2025-06-03 19:21:51 +0200

    añadí un colorpicker para personalizar la tarjeta al crearlo

--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -10,6 +10,8 @@ import { db } from "../firebase/database";
 import { Button } from 'primereact/button';
 import { Chips } from "primereact/chips";
 import { FloatLabel } from 'primereact/floatlabel';
+import { ProgressSpinner } from "primereact/progressspinner";
+import { ColorPicker } from "primereact/colorpicker";
 
 
 
@@ -20,16 +22,24 @@ export default function Accounts(){
     const [numberAccount, setNumberAccount] = useState(Math.floor(Math.random() * 10000000000000000));
     const [cards, setCards] = useState([]);
     const [owners, setOwners] = useState([]);
-
+    const [colorRGB, setColorRGB] = useState({ r: 100, g: 102, b: 241 });
+    const [colorCard, setColorCard] = useState();
     
 
     useEffect(() => {
         const fetchCards = async () => {
-          const q = query(collection(db, "ACCOUNTS"), where("owners", "array-contains", currentUser.email));
-          const querySnapshot = await getDocs(q);
-          const cardsData = querySnapshot.docs.map(doc => doc.data());
-          setCards(cardsData);
-        };
+            if (!currentUser?.email) return;
+
+            try{
+            const q = query(collection(db, "ACCOUNTS"), where("owners", "array-contains", currentUser.email));
+            const querySnapshot = await getDocs(q);
+            const cardsData = querySnapshot.docs.map(doc => doc.data());
+            setCards(cardsData);
+            } catch (e){
+                console.error("Error fechings accounts", e);
+                
+            }
+        }
     
         fetchCards();
       }, [currentUser.email, cards]);
@@ -59,6 +69,11 @@ export default function Accounts(){
         }
     }
 
+    const changeColorCard = (e) =>{
+        setColorRGB(e.value)
+        setColorCard(`${colorRGB.r},${colorRGB.g},${colorRGB.b}`)
+    }
+
     return(
         <>
             <section className="sm:grid md:flex">
@@ -67,9 +82,9 @@ export default function Accounts(){
                     <h1>ACCOUNTS</h1>
                     <div className="flex flex-wrap gap-7 py-5">
                         <div className="flex flex-wrap gap-4">
-                              {cards.map((card, index) => (
+                              {cards ? cards.map((card, index) => (
                                 <CardBank key={index} number={card.number} />
-                              ))}
+                              )) : <ProgressSpinner />}
                         </div>
                         <CardNew click={setShowForm} value={!showForm}/>
                         <div className={showForm ? "animate-slide-in-left flex flex-col gap-5 top-0 right-0 border absolute w-[230] h-full bg-white border-gray-300 shadow-lg  p-5 rounded-xl" : "hidden"}>
@@ -79,13 +94,13 @@ export default function Accounts(){
                                     <LucideX />
                                 </button>
                             </div>
-                            <CardBank title="Nueva tarjeta" number={numberAccount} createDate={new Date().toLocaleDateString()}/>
+                            <CardBank title="Nueva tarjeta" color={colorCard} redirect={false} number={numberAccount} createDate={new Date().toLocaleDateString()}/>
                             <FloatLabel>
-                                <Chips id="miembros" className="border" value={owners} onChange={(e) => setOwners(e.value)} />
+                                <Chips id="miembros" className="w-80 border" value={owners} onChange={(e) => setOwners(e.value)} />
                                 <label htmlFor="miembros">Miembros...</label>
                             </FloatLabel> 
+                            <ColorPicker format="rgb" value={colorRGB} onChange={changeColorCard} />
                             <button type="button" onClick={addAccountInDataBase} className="bg-black hover:bg-neutral-800 text-white py-4 px-8 rounded-xl">Crear nueva tarjeta</button>
-                            <Button label="Crear nueva cuenta" severity="primary" icon="pi pi-credit-card" text raised />
                             <Button label="Añadir cuenta existente" severity="secondary" icon="pi pi-credit-card" text raised />
                         </div>
                     </div>
