4f8abd7e67ebfa7f648f8d85c2496df1057f58e0
jgt <jgt@nextret.net>
2025-07-02 13:54:13 +0200

    se agrego una tabla y tags en la tabla de Loans mas sortables para los campos con selected que suma en el header la cantidad a pagar

--- src/pages/Loans.jsx
+++ src/pages/Loans.jsx
@@ -8,6 +8,10 @@ import 'primeicons/primeicons.css';
 import Header from "../components/Header.jsx";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { Tag } from 'primereact/tag';
+import { Toolbar } from "primereact/toolbar";
+import { Button } from "primereact/button";
+        
         
         
 
@@ -19,6 +23,7 @@ export default function Loans() {
     const [accountsData, setAccountsData] = useState([])
     const [accounts,  setAccounts] = useState([]);
     const [selectedAccount, setSelectedAccount] = useState(null);
+    const [selectedLoans, setSelectedLoans] = useState(null);
 
         useEffect(() => {
              const fetchLoans = async () => {
@@ -73,16 +78,81 @@ export default function Loans() {
             fetchAccounts();
           });
 
-          const mostrar = () => {
-            console.log(loans);
+          const amountRow = (rowData) => {
+            return (
+                <p>{Intl.NumberFormat("es-ES", {
+                      style: "currency",
+                      currency: "EUR",
+                    }).format(parseFloat(rowData.amount))}</p>
+            )
+          }
+
+          const statusRow = (rowData) => {
+            return(
+                <Tag pt={{root : {className : 'flex flex-row-reverse gap-1'}}} severity={getSeverity(rowData.status)} value={rowData.status} icon={getIconStatus(rowData.status)}></Tag>
+            )
+          }
+
+          const userRow = (rowData) => {
+            return(
+                <span className="flex flex-row gap-1">
+                    <img className="h-5 w-5 rounded-full" src={rowData.userPhoto} alt="Perfil del usuario" />
+                    <p className="truncate">{rowData.user}</p>
+                </span>
+            )
+          }
+
+          const getIconStatus = (status) => {
+            switch (status) {
+                case 'pendiente':
+                    return 'pi pi-exclamation-triangle'
+                case 'pagado':
+                    return 'pi pi-check-circle'
+                default:
+                    return 'pi pi-exclamation-circle'
+            }
+          }
+
+          const getSeverity = (status) => {
+            switch (status) {
+                case 'pendiente':
+                    return 'warning'
+                case 'pagado':
+                    return 'success'
+                default:
+                    return 'info'
+            }
+          }
+
+          const mostrarLog = () => {
+            console.log(selectedLoans);
             
           }
 
-          const mostrar2 = () => {
-            console.log(loans[0].number);
-            
+          const getButtonsOfAction = () => {
+            return(
+                <Button label="Pagar" icon="pi pi-dollar" severity="success" disabled={!selectedLoans || !selectedLoans.length} onClick={mostrarLog} ></Button>
+            )
           }
 
+          const getTotal = () => {
+            let total = 0;
+            if (selectedLoans) {
+                selectedLoans.forEach(item => {
+                total =  parseFloat(total) + parseFloat(item.amount)
+            });
+            }
+            return(
+                <p className="font-bold md:text-3xl">Cantidad a pagar: {Intl.NumberFormat("es-ES", {
+                      style: "currency",
+                      currency: "EUR",
+                    }).format(parseFloat(total))}</p>
+            )
+          }
+          
+          const sum = (a, b) => {
+            return a + b
+          }
     return(
         <>
             <div className="sm:grid md:flex">
@@ -100,13 +170,15 @@ export default function Loans() {
                         placeholder="Selecciona una cuenta" className="w-full md:w-14rem" />
                     </div>
                     
-                    <div hidden={loans[0]?.loans?.length > 0 && selectedAccount ? false : true} className="m-2 md:mt-2 bg-white border h-full border-gray-200 rounded-md p-5 flex gap-3 text-xl md:text-3xl text-gray-500">
-                        
-                            <DataTable rowCheckbox removableSort sortMode="multiple" selection={true} value={loans[0]?.loans} tableStyle={{ minWidth: '20rem' }}>
-                                <Column field="user" header="Nombre" sortable style={{ width: '25%' }}></Column>
-                                <Column field="amount" header="Monto" sortable style={{ width: '25%' }}></Column>
-                                <Column field="status" header="Estado" sortable style={{ width: '25%' }}></Column>
-                                <Column field="date" header="Fecha" sortable style={{ width: '25%' }}></Column>
+                    <div hidden={loans[0]?.loans?.length > 0 && selectedAccount ? false : true} className="m-2 md:mt-2 bg-white border h-full border-gray-200 rounded-md p-5 flex flex-col gap-3 text-xl md:text-3xl text-gray-500">
+                            <Toolbar left={getTotal} right={getButtonsOfAction}></Toolbar>
+                            <DataTable className="w-full" removableSort stripedRows selection={selectedLoans} onSelectionChange={(e) => setSelectedLoans(e.value)}
+                            sortField="date" sortOrder={-1} value={loans[0]?.loans}>
+                                <Column selectionMode="multiple" exportable={true}></Column>
+                                <Column field="user" header="Usuario" body={userRow} sortable></Column>
+                                <Column field="date" header="Fecha" sortable></Column>
+                                <Column field="amount" header="Monto" body={amountRow} sortable></Column>
+                                <Column field="status" header="Estado" body={statusRow} sortable></Column>
                             </DataTable>
                         
                     </div>
