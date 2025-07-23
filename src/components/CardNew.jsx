35868b3c0ccf3445ce82b8fcd68f1988f581dfbc
jgt <jgt@nextret.net>
2025-07-23 13:30:41 +0200

    cambio de diseño card para crear nueva tarjeta

--- src/components/CardNew.jsx
+++ src/components/CardNew.jsx
@@ -1,15 +1,8 @@
-import { Plus } from "lucide-react";
-
 export default function CardNew({click, value, visible}) {
   return (
     <div onClick={() => click(value)}
-    className="bg-white font-[Trebuchet_MS] w-[91vw] sm:w-auto md:max-w-[313px] transition-all  ease-in hover:rotate-[1deg] hover:rotate-y-[10deg] hover:shadow-[0_7px_7px_#eee] h-[210px] md:h-[200px] duration-200 hover:scale-105 hover:border-gray-700 py-20 px-36 border-1 cursor-pointer border-dashed border-neutral-400 rounded-2xl">
-      <Plus
-        size={100}
-        strokeWidth={0.5}
-        className="text-neutral-400 w-full h-full"
-      />
-      <p className="select-none text-neutral-400 text-center">Crear</p>
+    className="bg-white font-[Trebuchet_MS] w-[91vw] sm:w-auto md:max-w-[313px] transition-all flex flex-col justify-center items-center ease-in hover:rotate-[1deg] hover:rotate-y-[10deg] hover:shadow-[0_7px_7px_#eee] h-[210px] md:h-[200px] duration-200 hover:scale-105 hover:border-gray-700 py-20 px-36 border-1 cursor-pointer border-dashed border-neutral-300 rounded-2xl">
+      <p className="select-none text-neutral-300 text-center w-[313px]">Crear nueva tarjeta</p>
     </div>
   );
 }
