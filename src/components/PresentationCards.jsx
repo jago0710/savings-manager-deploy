07207f69c08d1c3e7bf1f8375e3d97df98832e23
jgt <jgt@nextret.net>
2025-07-23 14:01:10 +0200

    add FieldSet de primereact para la presentación de los apartados como guía

---
+++ src/components/PresentationCards.jsx
@@ -0,0 +1,54 @@
+import {Fieldset} from 'primereact/fieldset'
+
+export default function PresentationCards (){
+ 
+    const legendTemplate = (
+        <div className="flex align-items-center gap-2 px-2">
+            <span className="font-bold">Amy Elsner</span>
+        </div>
+    );
+ 
+    return(
+        <>
+            
+
+            <section className='flex flex-col gap-2'>
+                <Fieldset toggleable legend={legendTemplate}>
+                    <p className="m-0">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </Fieldset>
+
+                <Fieldset toggleable legend={legendTemplate}>
+                    <p className="m-0">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </Fieldset>
+
+                <Fieldset toggleable legend={legendTemplate}>
+                    <p className="m-0">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </Fieldset>
+            
+                <Fieldset toggleable legend={legendTemplate}>
+                    <p className="m-0">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </Fieldset>
+            </section>
+        </>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Navbar from "../components/Navbar"
+import PresentationCards from "../components/PresentationCards";
 import useUser from "../hook/useUser"
 
 export default function Home(){
@@ -13,8 +14,9 @@ console.log("=> ", user);
                 <Navbar page="INICIO"/>
                 <div className="md:ml-67 bg-gray-50 w-full h-[calc(100vh-74px)]">
                   <Header title="INICIO"></Header>
-                  <div className="mt-18 bg-white w-[calc(100% - 4px)] md:mt-2 m-2 h-screen border border-gray-200 rounded-md flex justify-center items-center">
-                    <h1 className="text-3xl md:text-4xl">EN MANTENIMIENTO...</h1>
+                  <div className="mt-18 bg-white w-[calc(100% - 4px)] md:mt-2 m-2 h-screen border border-gray-200 rounded-md flex justify-center items-center p-2">
+                    <h1 hidden className="text-3xl md:text-4xl">EN MANTENIMIENTO...</h1>
+                    <PresentationCards></PresentationCards>
                   </div>
                 </div>
            </div>
