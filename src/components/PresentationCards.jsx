0723bb42440efb564b4f9b5f26f6e0cccb371c2e
jgt <jgt@nextret.net>
2025-07-24 14:13:38 +0200

    add doc of section account

--- src/components/PresentationCards.jsx
+++ src/components/PresentationCards.jsx
@@ -1,42 +1,61 @@
-import {Fieldset} from 'primereact/fieldset'
+import { Accordion, AccordionTab } from 'primereact/accordion';
 
 export default function PresentationCards (){
  
-    const legendTemplate = (
-        <div className="flex align-items-center gap-2 px-2">
-            <span className="font-bold">Amy Elsner</span>
-        </div>
-    );
  
     return(
         <>
-            
+            <Accordion activeIndex={0}>
+                <AccordionTab header="Introducción">
+                    <p>Para poder saber como funciona la aplicacío puedes consultar una guía por cada sección de la aplicación.</p>
+                    <p className='mt-2'>En estas secciones se explica el funcionamiento y conceptos que se tienen que tener en cuenta para poder evitar errores a futuro</p>
+                    <p className='mt-2'></p>
+                </AccordionTab>
+                <AccordionTab header="Guía de cuentas">
+                    <section className='m-0 flex flex-col gap-7'>
+                        <div className='flex flex-col md:flex-row gap-5'>
+                            <img src="./assets/accounts/NewCard.png" className='w-60 border border-gray-200 rounded-md' alt="new-card" />  
+                            <p>Una vez nos encontremos en la sección de <b>Cuentas</b> podremos ver una tarjeta que pone <b>Crear nueva tarjeta</b> presionamos la tarjeta para que nos salga la ventana donde se crean las tarjetas.</p>    
+                        </div>
+                        <div className='flex flex-col-reverse md:flex-row gap-3'>
+                            <div>
+                                <p>Una vez presionado nos saldrá la siguiente ventana por la derecha, en esta ventana tendremos que rellenar los datos con los que queremos crear esta nueva tarjeta, como lo siguientes campos:</p>
 
-            <section className='flex flex-col gap-2'>
-                <Fieldset toggleable legend={legendTemplate}>
+                                <h2 className='mt-5 mb-1.5 text-lg font-semibold text-black'>Selector de color</h2>
+                                <p>El <b>selector de color</b> es para que se pueda elegir a gusto el color de la card y asi tenerlo personalizado</p>
+                                <h2 className='mt-5 mb-1.5 text-lg font-semibold text-black'>Campo descripción</h2>
+                                <p>El campo <b>descripción</b> se debe de poner un nombre de la tarjeta para el tipo de ahorro por ejemplo <b>Viajes</b>, 
+                                poniendo esto es más facil de identificarlo entre las tarjetas</p>
+                                <h2 className='mt-5 mb-1.5 text-lg font-semibold text-black'>Campo objetivo</h2>
+                                <p>En el campo objetivo se ingresa la cantidad de dinero que tienes como objetivo en tus ahorros y asi poder mostrar en tiempo real 
+                                   el porcentaje que te falta para llegar a este objetivo puesto, se puede visualizar en dashboard</p>
+                                 <h2 className='mt-5 mb-1.5 text-lg font-semibold text-black'>Campo miembros</h2>
+                                 <p>Este campo es uno de los más importantes, ya que se tiene que poner</p>
+                            </div>
+                            <img src="./assets/accounts/FormNewCard.png" className='w-60 border border-gray-200 rounded-md' alt="form-new-card" />
+                        </div>
+                            <div>
+                                <img src="./assets/accounts/CreateCard.png" className='w-120 border border-gray-200 rounded-md' alt="new-card" />
+                            </div>
+                    </section>
+                </AccordionTab>
+                <AccordionTab header="Guía de prestamos">
                     <p className="m-0">
-                        Guía completa
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                        quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+                        sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                        Consectetur, adipisci velit, sed quia non numquam eius modi.
                     </p>
-                </Fieldset>
-
-                <Fieldset toggleable legend={legendTemplate}>
-                    <p className="m-0">
-                        Guía completa
-                    </p>
-                </Fieldset>
-
-                <Fieldset toggleable legend={legendTemplate}>
-                    <p className="m-0">
-                        Guía completa
-                    </p>
-                </Fieldset>
-            
-                <Fieldset toggleable legend={legendTemplate}>
-                    <p className="m-0">
-                        Guía completa
-                    </p>
-                </Fieldset>
-            </section>
+                </AccordionTab>
+                <AccordionTab header="Guía de Dashboard">
+                    <section id='guia-dashboard' className="m-0">
+                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
+                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt
+                        mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
+                        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+                    </section>
+                </AccordionTab>
+            </Accordion>
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ console.log("=> ", user);
                 <Navbar page="INICIO"/>
                 <div className="md:ml-67 bg-gray-50 w-full h-[calc(100vh-74px)]">
                   <Header title="INICIO"></Header>
-                  <div className="mt-18 bg-white w-[calc(100% - 4px)] md:mt-2 m-2 h-screen border border-gray-200 rounded-md flex justify-center items-center p-2">
+                  <div className="mt-18 bg-white w-[calc(100% - 4px)] md:mt-2 m-2  border border-gray-200 rounded-md flex justify-center items-center p-2">
                     <h1 hidden className="text-3xl md:text-4xl">EN MANTENIMIENTO...</h1>
                     <PresentationCards></PresentationCards>
                   </div>
