214c53117871e4b09807bc798844a41d9468bad0
jgt <jgt@nextret.net>
2025-07-31 13:52:07 +0200

    images de guia para prestamos

--- src/components/PresentationCards.jsx
+++ src/components/PresentationCards.jsx
@@ -41,12 +41,41 @@ export default function PresentationCards (){
                     </section>
                 </AccordionTab>
                 <AccordionTab header="Guía de prestamos">
-                    <p className="m-0">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                        quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
-                        sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-                        Consectetur, adipisci velit, sed quia non numquam eius modi.
-                    </p>
+                    <section className='m-0 flex flex-col gap-10'>
+                        <h1 className='text-xl font-semibold'>¿Como pedir prestamos?</h1>
+                        <div className='flex flex-col-reverse md:flex-row gap-7 border-b border-gray-200 pb-10'>
+                            <img src="./assets/accounts/NewCard.png" className='w-70 border object-cover border-gray-200 rounded-md' alt="new-card" />  
+                            <div className='flex flex-col justify-center'> 
+                                <h1 className='text-xl font-semibold'>Paso 1</h1> 
+                                <p>Una vez nos encontremos en la sección de <b>Cuentas</b> podremos ver una tarjeta que pone <b>Crear nueva tarjeta</b> presionamos la tarjeta para que nos salga la ventana donde se crean las tarjetas.</p>
+                            </div>  
+                        </div>
+
+                        <div className='flex flex-col-reverse md:flex-row gap-7 border-b border-gray-200 pb-10'>
+                            <img src="./assets/accounts/NewCard.png" className='w-70 border object-cover border-gray-200 rounded-md' alt="new-card" />  
+                            <div className='flex flex-col justify-center'> 
+                                <h1 className='text-xl font-semibold'>Paso 2</h1> 
+                                <p>Una vez nos encontremos en la sección de <b>Cuentas</b> podremos ver una tarjeta que pone <b>Crear nueva tarjeta</b> presionamos la tarjeta para que nos salga la ventana donde se crean las tarjetas.</p>
+                            </div>  
+                        </div>
+
+                        <div className='flex flex-col-reverse md:flex-row gap-7 border-b border-gray-200 pb-10'>
+                            <img src="./assets/accounts/NewCard.png" className='w-70 border object-cover border-gray-200 rounded-md' alt="new-card" />  
+                            <div className='flex flex-col justify-center'> 
+                                <h1 className='text-xl font-semibold'>Paso 3</h1> 
+                                <p>Una vez nos encontremos en la sección de <b>Cuentas</b> podremos ver una tarjeta que pone <b>Crear nueva tarjeta</b> presionamos la tarjeta para que nos salga la ventana donde se crean las tarjetas.</p>
+                            </div>  
+                        </div>
+
+                        <h1 className='text-xl font-semibold'>¿Como pagar prestamos?</h1>
+                        <div className='flex flex-col-reverse md:flex-row gap-7 border-b border-gray-200 pb-10'>
+                            <img src="./assets/accounts/NewCard.png" className='w-70 border object-cover border-gray-200 rounded-md' alt="new-card" />  
+                            <div className='flex flex-col justify-center'> 
+                                <h1 className='text-xl font-semibold'>Paso 1</h1> 
+                                <p>Una vez nos encontremos en la sección de <b>Cuentas</b> podremos ver una tarjeta que pone <b>Crear nueva tarjeta</b> presionamos la tarjeta para que nos salga la ventana donde se crean las tarjetas.</p>
+                            </div>  
+                        </div>
+                    </section>
                 </AccordionTab>
                 <AccordionTab header="Guía de Dashboard">
                     <section id='guia-dashboard' className="m-0">
