c4e03da2ad7994478c5e95e4fffc34e3df87ae81
jgt <jgt@nextret.net>
2025-09-04 12:44:46 +0200

    mejora de subtitulo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ console.log("=> ", user);
                   <Header title="INICIO"></Header>
                   <div className="mt-16 md:mt-0 mx-3">
                     <h1 className="text-2xl mb-2" >Bienvenido/a a <b className="text-red-700 underline welcome">Savings Bank</b></h1>
-                    <p>Gestiona tus finanzas de manera inteligente y segura.</p>
+                    <p>Gestiona tus finanzas de manera inteligente y segura con nuestra aplicación.</p>
                   </div>
                   <div className="flex flex-col sm:flex-row w-[calc(100% - 4px)] md:mt-2 m-2 gap-2">
                     <div className="mt-0 bg-white w-full h-50 border border-gray-200 rounded-md flex justify-between p-2 ">
