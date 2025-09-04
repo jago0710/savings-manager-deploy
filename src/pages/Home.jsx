8be8e6d4cfcd09aaa0ae6730388e988b963be669
jgt <jgt@nextret.net>
2025-09-04 14:05:09 +0200

    mejora de inicio / Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,13 +13,22 @@ console.log("=> ", user);
                 <Navbar page="INICIO"/>
                 <div className="md:ml-67 bg-gray-50 w-full h-[calc(100vh-74px)]">
                   <Header title="INICIO"></Header>
-                  <div className="mt-16 md:mt-0 mx-3">
-                    <h1 className="text-2xl mb-2" >Bienvenido/a a <b className="text-red-700 underline welcome">Savings Bank</b></h1>
-                    <p>Gestiona tus finanzas de manera inteligente y segura con nuestra aplicación.</p>
+                  <div className="mt-18 md:mt-2 mx-2 rounded-md bg-white w-[calc(100% - 4px)] border border-gray-200 p-2">
+                    <p className="text-2xl" >Bienvenido a <b className="text-red-700 welcome"><a href="/home"><span className="mr-0.5">/</span>Savings Bank</a></b></p>
                   </div>
                   <div className="flex flex-col sm:flex-row w-[calc(100% - 4px)] md:mt-2 m-2 gap-2">
                     <div className="mt-0 bg-white w-full h-50 border border-gray-200 rounded-md flex justify-between p-2 ">
-                      
+                      {/** COMPONENTE */}
+                      <div className="border h-15 w-full rounded-md border-gray-100 hover:bg-blue-100 flex flex-row hover:cursor-crosshair">
+                        <div className="p-2 w-15 flex justify-center items-center">
+                          icon
+                        </div>
+                        <div className="flex flex-col justify-center">
+                          <h1>Title</h1>
+                          <p>Description card</p>
+                        </div>
+                      </div>
+                      {/** FIN DE COMPONENTE */}
                     </div>
                     <div className="mt-0 bg-white w-full h-50 border border-gray-200 rounded-md flex justify-between p-2 ">
                       
