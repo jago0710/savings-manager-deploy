5958c8b133f6449327d2bb1acae2d7e9f2075d16
jgt <jgt@nextret.net>
2025-09-09 10:03:48 +0200

    add news RedirectButtons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { ChartColumnIncreasing, CreditCard, PiggyBank } from "lucide-react";
+import { ChartColumnIncreasing, CreditCard, HelpCircle, PiggyBank, Plus, Settings } from "lucide-react";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import RedirectButton from "../components/RedirectButton";
@@ -20,15 +20,19 @@ console.log("=> ", user);
                   </div>
                   <div className="flex flex-col sm:flex-row w-[calc(100% - 4px)] md:mt-2 m-2 gap-2">
                     <div className="mt-0 bg-white w-full  border border-gray-200 rounded-md flex flex-col justify-between gap-2 p-5 ">
-                      <h1>Acciones Rápidas</h1>
+                      <h1 className="text-xl mb-4 font-semibold">Acciones Rápidas</h1>
                       {/** COMPONENTE */}
                       <RedirectButton href="/accounts" title="Gestionar Cuentas" description="Ver y administrar todas tus cuentas" icon={CreditCard} BorderColor="rgba(238, 32, 32, 0.8)" backgroundColor="rgba(238, 32, 32, 0.04)"/>
                       <RedirectButton href="/loans" title="Ver Préstamos" description="Consulta el estado de tus préstamos" icon={PiggyBank}/>
                       <RedirectButton href="/dashboard" title="Ir al Dashboard" description="Análisis y reportes financieros" icon={ChartColumnIncreasing}/>
                       {/** FIN DE COMPONENTE */}
                     </div>
-                    <div className="mt-0 bg-white w-full border border-gray-200 rounded-md flex justify-between p-2 ">
-                      
+                    <div className="mt-0 bg-white w-full  border border-gray-200 rounded-md flex flex-col justify-between gap-2 p-5 ">
+                      <h1 className="text-xl mb-4 font-semibold">Acciones Rápidas</h1>
+
+                      <RedirectButton href="/accounts" title="Configuración" description="Personaliza tu experiencia" icon={Settings} BorderColor="rgba(238, 32, 32, 0.8)" backgroundColor="rgba(238, 32, 32, 0.04)"/>
+                      <RedirectButton href="/loans" title="Centro de ayuda" description="Soporte y documentación guía" icon={HelpCircle}/>
+                      <RedirectButton href="/dashboard" title="Agregar Nueva Cuenta" description="Crea una nueeva cuenta" icon={Plus}/>
                     </div>
                   </div>
                   <div className="bg-white w-[calc(100% - 4px)] h-[calc(100vh-292px)] md:mt-2 m-2 border border-gray-200 rounded-md flex justify-between items-center p-2">
