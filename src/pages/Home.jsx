af63465d15dea971aaa590fcf257011a3d46d368
jgt <jgt@nextret.net>
2025-10-09 13:15:20 +0200

    Cambio de fallos ortográficos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,17 +17,17 @@ console.log("=> ", user);
                   <Header title="INICIO"></Header>
                   <div className="flex flex-col sm:flex-row w-[calc(100% - 4px)] md:mt-2 m-2 gap-2">
                     <div className="md:mt-0 bg-white w-full border border-gray-200 rounded-md flex flex-col justify-between gap-3 p-4 mt-16">
-                      <h1 className="text-xl mb-1 font-semibold">Acciones Rápidas</h1>
+                      <h1 className="text-xl mb-1 font-semibold">Acciones rápidas</h1>
                       
-                      <RedirectButton href="/accounts" title="Gestionar Cuentas" description="Realizar y ver movimientos" icon={CreditCard} BorderColor="rgba(0, 158, 25, 0.5)" backgroundColor="rgba(0, 224, 35, 0.03)" iconColor="rgba(0, 158, 35, 0.8)"/>
-                      <RedirectButton href="/loans" title="Ver Préstamos" description="Ver estado de prestamos" icon={HandCoins} BorderColor="rgba(247, 89, 6, 0.5)" backgroundColor="rgba(247, 89, 6, 0.03)" iconColor="rgba(247, 89, 6, 0.8)"/>
-                      <RedirectButton href="/dashboard" title="Ir al Dashboard" description="Ver análisis financiero" icon={ChartColumnIncreasing} BorderColor="rgba(4, 87, 246, 0.5)" backgroundColor="rgba(4, 87, 246, 0.03)" iconColor="rgba(4, 87, 246, 0.8)"/>
+                      <RedirectButton href="/accounts" title="Gestionar cuentas" description="Realizar y ver movimientos" icon={CreditCard} BorderColor="rgba(0, 158, 25, 0.5)" backgroundColor="rgba(0, 224, 35, 0.03)" iconColor="rgba(0, 158, 35, 0.8)"/>
+                      <RedirectButton href="/loans" title="Ver préstamos" description="Ver estado de prestamos" icon={HandCoins} BorderColor="rgba(247, 89, 6, 0.5)" backgroundColor="rgba(247, 89, 6, 0.03)" iconColor="rgba(247, 89, 6, 0.8)"/>
+                      <RedirectButton href="/dashboard" title="Ir al dashboard" description="Ver análisis financiero" icon={ChartColumnIncreasing} BorderColor="rgba(4, 87, 246, 0.5)" backgroundColor="rgba(4, 87, 246, 0.03)" iconColor="rgba(4, 87, 246, 0.8)"/>
                      
                     </div>
                     <div className="mt-0 bg-white w-full border border-gray-200 rounded-md flex flex-col justify-between gap-3 p-4 sm:mt-16 md:mt-0">
                       <h1 className="text-xl mb-1 font-semibold">Funciones adicionales</h1>
 
-                      <RedirectButton href="/accounts/true" title="Agregar Nueva Cuenta" description="Crea una nueva cuenta" icon={Plus} iconColor="rgba(7, 120, 250, 0.8)" BorderColor="rgba(7, 120, 250, 0.5)" backgroundColor="rgba(7, 120, 250, 0.03)"/>
+                      <RedirectButton href="/accounts/true" title="Agregar nueva cuenta" description="Crea una nueva cuenta" icon={Plus} iconColor="rgba(7, 120, 250, 0.8)" BorderColor="rgba(7, 120, 250, 0.5)" backgroundColor="rgba(7, 120, 250, 0.03)"/>
                       <RedirectButton href="/settings" title="Configuración" description="Personaliza tu experiencia" icon={Settings} iconColor="rgba(7, 120, 250, 0.8)" BorderColor="rgba(7, 120, 250, 0.5)" backgroundColor="rgba(7, 120, 250, 0.03)" />
                       <RedirectButton href="/help" title="Centro de ayuda" description="Soporte y documentación" icon={HelpCircle} iconColor="rgba(7, 120, 250, 0.8)" BorderColor="rgba(7, 120, 250, 0.5)" backgroundColor="rgba(7, 120, 250, 0.03)"/>
 
